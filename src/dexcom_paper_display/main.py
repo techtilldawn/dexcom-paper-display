ca7f181f3d4f3bfc809690a0d41e80a709f08d2d
techtilldawn <michael.biro@outlook.de>
2026-08-13 10:22:21 +0200

    (add) show actual info in display

--- src/dexcom_paper_display/main.py
+++ src/dexcom_paper_display/main.py
@@ -31,20 +31,11 @@ def main():
     rootDir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'ext')
 
     font15 = ImageFont.truetype(os.path.join(rootDir, '0xProtoNerdFontMono-Regular.ttf'), 15)
-    
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
     draw = ImageDraw.Draw(image)
     
-    draw.rectangle([(0,0),(50,50)],outline = 0)
-    draw.rectangle([(55,0),(100,50)],fill = 0)
-    draw.line([(0,0),(50,50)], fill = 0,width = 1)
-    draw.line([(0,50),(50,0)], fill = 0,width = 1)
-    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
-    draw.ellipse((55, 60, 95, 100), outline = 0)
-    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
-    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
-    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
-    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
-    draw.text((120, 60), 'e-Paper demo', font = font15, fill = 0)
+    draw.text((120, 60), glucose_reading, font = font15, fill = 0)
     epd.display(epd.getbuffer(image))
-    epd.sleep()
+    epd.sleep(5)
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+
